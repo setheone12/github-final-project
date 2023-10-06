129d8d4997ed4f749aa0b3cdbe0530a531f27667
setheone12 <102647627+setheone12@users.noreply.github.com>
2023-10-06 15:24:40 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <rldizon1891@gmail.com>
+# <setheone12>
 # Input:
 # p, principal amount
 # t, time period in years
